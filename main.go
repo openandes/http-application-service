c99117b39bac92d212ea06804bb19bf84e6e20d3
Alabama System Control One <alabama.system@gmail.com>
2025-12-01 01:32:29 -0300

    feat: loggin user IP address

--- main.go
+++ main.go
@@ -6,9 +6,15 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
-		log.Println("South Labs")
+	http.HandleFunc("/", func(responsewriter http.ResponseWriter, request *http.Request) {
+		log.Printf("[i] Client Data IP Address %s\n", request.RemoteAddr)
 	})
 
-	http.ListenAndServe(":9090", nil)
+	e := func() error {
+		return http.ListenAndServe(":80", nil)
+	}()
+
+	if e != nil {
+		log.Fatalf("something just happened... %v", e)
+	}
 }
